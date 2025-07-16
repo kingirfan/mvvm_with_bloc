63f570016d09fb975d1e3e52ef7fff12e5e0f62d
irfan <irfuu.shariff@gmail.com>
2025-07-16 20:27:34 +0530

    Home screen till selection

--- lib/feature/nav_screen/home/presentation/view_models/HomePageViewModel.dart
+++ lib/feature/nav_screen/home/presentation/view_models/HomePageViewModel.dart
@@ -6,48 +6,6 @@ import '../../domain/usecase/category_usecase.dart';
 import '../bloc/home_bloc.dart';
 import '../bloc/home_event.dart';
 
-/*class HomePageViewModel {
-  List<CategoryModel> categories = [];
-  bool isLoading = false;
-  String? error;
-
-  CategoryModel? currentCategory;
-  var selectedCategory;
-
-  void loadCategories(BuildContext context) {
-    final bloc = BlocProvider.of<HomePageBloc>(context);
-    bloc.add(LoadCategoriesEvent());
-  }
-
-  void handleState(HomePageState state) {
-    if (state is HomePageLoading) {
-      isLoading = true;
-      error = null;
-    } else if (state is HomePageCategoryLoaded) {
-      isLoading = false;
-      categories = state.categories;
-      selectedCategory = state.selectedCategory;
-
-      error = null;
-    } else if (state is HomePageFailure) {
-      isLoading = false;
-      error = state.error.message;
-    }
-  }
-
-  void selectCategory(CategoryModel category) {
-    // If already selected, do nothing
-    if (currentCategory?.id == category.id) return;
-
-    currentCategory = category;
-
-    // If you later want to fetch items based on selection:
-    // if (category.items.isEmpty) {
-    //   fetchItemsForCategory(category);
-    // }
-  }
-
-}*/
 
 class HomePageViewModel {
   bool _hasAutoSelected = false;
